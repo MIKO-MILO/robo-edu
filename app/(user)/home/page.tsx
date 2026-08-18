f8a35c29162119a44dd9b274bde6d84ab832d304
ziinset <goldiamukti@gmail.com>
2026-08-18 22:32:15 +0700

    feat: making a new comp navbar

---
+++ app/(user)/home/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "@/components/user/navbar";
+
+export default function HomePage() {
+  return (
+    <main className="min-h-screen bg-background">
+      <Navbar />
+      {/* Page content goes here */}
+    </main>
+  );
+}
