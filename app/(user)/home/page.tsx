8cfd7e1bdb823a7c5901d9533b3ee2b7782430a7
ziinset <goldiamukti@gmail.com>
2026-08-19 11:30:48 +0700

    feat: new component footer

--- app/(user)/home/page.tsx
+++ app/(user)/home/page.tsx
@@ -1,11 +1,26 @@
 import Navbar from "@/components/user/navbar";
 import CarouselLogo from "@/components/user/carousel-logo";
+import Footer from "@/components/user/footer";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-background">
       <Navbar />
-      <CarouselLogo />
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      gggggggggggggggggggggggggggggggg<br></br>bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb
+      <Footer />
       {/* Page content goes here */}
     </main>
   );
